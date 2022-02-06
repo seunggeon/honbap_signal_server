b1df813e15b8bf161b4403e0b12632d4eba8e164
deong <kodd1102@naver.com>
2022-02-07 01:48:45 +0900

    add [signal Off] and [delete signal]

--- src/app/Signal/signalRoute.js
+++ src/app/Signal/signalRoute.js
@@ -14,4 +14,11 @@ module.exports = function (app) {
 
     // 시그널 매칭 잡혔을 때
     app.patch("/signal/:userIdx/list/matching", signal.postSigMatch);
+
+    // 시그널 OFF
+    app.patch("/signal/:userIdx/list/off", signal.patchSigStatus);
+
+    // 시그널 삭제
+    app.delete("/signal/:userIdx/list", signal.deleteSignal);
+
 };
