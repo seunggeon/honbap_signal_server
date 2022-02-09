24b3cdd08e29e9837c50768266511b458ab8e0c6
deong <kodd1102@naver.com>
2022-02-10 02:05:06 +0900

    complete comment side

--- src/app/Comment/commentDao.js
+++ src/app/Comment/commentDao.js
@@ -1,5 +1,7 @@
 // Comment
 
+const { startTimer } = require("winston");
+
 // 후기 등록
 async function insertComment(connection, params) {
     const query =   `
@@ -7,37 +9,29 @@ async function insertComment(connection, params) {
                     (signalIdx, userIdx, writerIdx, comment, star)
                     VAlUES (?, ?, ?, ?, ?);
                     `;
-    const [row] = connection.query(query, params);
+    const [row] = await connection.query(query, params);
     return row;
 }
 
 // 나에게 써진 후기 조회
 async function selectCommented(connection, userIdx) {
     const query =   `
-                    SELECT up.nickName AS reviewer, c.comment, c.star
-                    FROM
-                        (SELECT nickName FROM UserProfile AS up, Comment AS c
-                            WHERE up.userIdx = c.writerIdx) AS up,
-                        Comment AS c
-                    WHERE c.userIdx = ?
-                    ORDER BY c.signalIdx DESC;
+                    SELECT nickName AS reviewer, comment, star
+                    FROM UserProfile AS up, Comment AS c
+                    WHERE c.userIdx = ? AND up.userIdx = c.writerIdx;
                     `;
-    const [row] = connection.query(query, userIdx);
+    const [row] = await connection.query(query, userIdx);
     return row;
 }
 
 // 내가 쓴 후기 조회
 async function selectCommenting(connection, writerIdx) {
     const query =   `
-                    SELECT up.nickName, c.comment, c.star
-                    FROM 
-                        (SELECT nickName FROM UserProfile AS up, Comment AS c
-                            WHERE up.userIdx = c.writerIdx) as up,
-                        Comment AS c
-                    WHERE c.writerIdx = ?
-                    ORDER BY c.signalIdx DESC;
+                    SELECT nickName AS reviewer, comment, star
+                    FROM UserProfile AS up, Comment AS c
+                    WHERE c.writerIdx = ? AND up.userIdx = c.userIdx;
                     `;
-    const [row] = connection.query(query, writerIdx);
+    const [row] = await connection.query(query, writerIdx);
     return row;
 }
 
