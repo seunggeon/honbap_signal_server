382a03fa6c3920d9ab7d31e480fceb60a017bfdb
= <=>
2022-02-11 23:17:21 +0900

    add jwt

--- config/jwtMiddleware.js
+++ config/jwtMiddleware.js
@@ -1,38 +1,36 @@
-const jwt = require('jsonwebtoken');
-const secret_config = require('./secret');
-const { response } = require("./response")
 const { errResponse } = require("./response")
 const baseResponse = require("./baseResponseStatus");
+const jwt = require('../modules/jwt');
 
-
+// route로 보내기 전에 검증만. 생성 X
 const jwtMiddleware = (req, res, next) => {
     // read the token from header or url
-    const token = req.headers['x-access-token'] || req.query.token;
-    // token does not exist
-    if(!token) {
-        return res.send(errResponse(baseResponse.TOKEN_EMPTY))
-    }
-
-    // create a promise that decodes the token
-    const p = new Promise(
-        (resolve, reject) => {
-            jwt.verify(token, secret_config.jwtsecret , (err, verifiedToken) => {
-                if(err) reject(err);
-                resolve(verifiedToken)
-            })
+        const token = req.headers['x-access-token'] || req.query.token;
+        // token does not exist
+        if(!token) {
+            return res.send(errResponse(baseResponse.TOKEN_EMPTY))
         }
-    );
 
-    // if it has failed to verify, it will return an error message
-    const onError = (error) => {
-        return res.send(errResponse(baseResponse.TOKEN_VERIFICATION_FAILURE))
-    };
-    // process the promise
-    p.then((verifiedToken)=>{
-        //비밀 번호 바뀌었을 때 검증 부분 추가 할 곳
-        req.verifiedToken = verifiedToken;
-        next();
-    }).catch(onError)
+        // create a promise that decodes the token
+        const p = new Promise(
+            (resolve, reject) => {
+                jwt.verify(token,(err, verifiedToken) => {
+                    if(err) reject(err);
+                    resolve(verifiedToken)
+                })
+            }
+        );
+
+        // if it has failed to verify, it will return an error message
+        const onError = (error) => {
+            return res.send(errResponse(baseResponse.TOKEN_VERIFICATION_FAILURE))
+        };
+        // process the promise
+        p.then((verifiedToken)=>{
+            // 검증 완료된 token -> request에 넣어서 route로 전송.
+            req.verifiedToken = verifiedToken;
+            next();
+        }).catch(onError)
 };
 
 module.exports = jwtMiddleware;
--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -1,46 +1,44 @@
 module.exports = function (app) {
     const user = require("./userController");
+    const jwtMiddleware = require("../../../config/jwtMiddleware");
     const passport = require("passport");
-    //const jwtMiddleware = require("../../../config/jwtMiddleware");
+ 
     //client와 통신 부분.
   
     // 1. 로그인 API (비회원일 경우 회원가입 후 로그인)
-    app.post("/user/login", function(req, res)
-    {
-        user.createUser
-    });
+    app.post("/user/login", 
+        user.createUser);
 
     // 2. 회원가입 (유저 개인정보) API
-    app.post("/user/signup", function(req, res){
+    app.post("/user/signup",
         user.postUsers
-    });
+    );
 
     // 3. 유저 프로필 등록 API
-    app.post("/user/signup/plusinfo", function(req, res){
+    app.post("/user/signup/plusinfo", jwtMiddleware,
         user.postUserProfile
-    });
+    );
 
     // 4. 유저 인덱스 조회 API
-    app.get("/user/signup/:userId", user.getUserIdx);
+    app.get("/user/signup/:userId", jwtMiddleware, user.getUserIdx);
 
     // 5. 유저 개인정보 조회 API
-    app.get("/user/myinfo/:userIdx", user.getUserInfo);
+    app.get("/user/myinfo/:userIdx", jwtMiddleware, user.getUserInfo);
 
     // 6. 유저 프로필 조회 (마이페이지) API
-    app.get("/user/mypage/:userIdx", user.getUserProfile);
+    app.get("/user/mypage/:userIdx", jwtMiddleware, user.getUserProfile);
 
     // 7. 유저 패스워드 수정 API
-    app.patch("/user/myinfo/:userIdx/modifypw", user.patchUserPassword);
+    app.patch("/user/myinfo/:userIdx/modifypw", jwtMiddleware, user.patchUserPassword);
 
     // 8. 유저 개인정보 수정 API
-    app.patch("/user/myinfo/:userIdx", user.patchUserInfo);
+    app.patch("/user/myinfo/:userIdx", jwtMiddleware, user.patchUserInfo);
 
     // 9. 유저 프로필 수정 (마이페이지) API
-    app.patch("/user/mypage/:userIdx", user.patchUserProfile);
+    app.patch("/user/mypage/:userIdx", jwtMiddleware, user.patchUserProfile);
 
     // 10. 카카오 로그인 API
     app.get('/auth/kakao', passport.authenticate('kakao-login')); 
     app.get('/auth/kakao/callback', passport.authenticate('kakao-login', { failureRedirect: '/user/login', successRedirect: '/home' }));
-
 
 };
