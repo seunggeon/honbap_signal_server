449f44a9877937e527e08e72359654754643f93b
seongddiyong <kodd1102@naver.com>
2022-03-31 16:04:14 +0900

    finish insert report API

---
+++ src/app/Report/reportService.js
@@ -0,0 +1,31 @@
+const { logger } = require("../../../config/winston");
+const { pool } = require("../../../config/database");
+const secret_config = require("../../../config/secret");
+
+//const reportProvider = require("./reportProvider");
+const reportDao = require("./reportDao");
+
+const baseResponse = require("../../../config/baseResponseStatus");
+const { response } = require("../../../config/response");
+const { errResponse } = require("../../../config/response");
+
+const jwt = require("jsonwebtoken");
+const { connect } = require("http2");
+
+
+// 시그널 등록
+exports.createReport = async function (userIdx, reportedIdx, shortReason, specificReason) {
+    try {
+        const params = [userIdx, reportedIdx, shortReason, specificReason];
+        const connection = await pool.getConnection(async (conn) => conn);
+        
+        const createSignalResult = await reportDao.insertReport(connection, params);
+
+        connection.release();
+        return response(baseResponse.SUCCESS);
+    }
+    catch (err) {
+        logger.error(`App - createReport Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+}
