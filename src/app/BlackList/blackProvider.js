c9c577cb263151b34fffeafb271b02b539c6e806
wjlee611 <wjlee611@gmail.com>
2022-06-30 23:00:26 +0900

    Fix provider baseresponse require

--- src/app/BlackList/blackProvider.js
+++ src/app/BlackList/blackProvider.js
@@ -1,49 +1,50 @@
 const { pool } = require("../../../config/database");
 const { errResponse } = require("../../../config/response");
 const { logger } = require("../../../config/winston");
+const baseResponse = require("../../../config/baseResponseStatus");
 
 const blackDao = require("./blackDao");
 
 // 블랙리스트 조회
 exports.getBlackList = async function (userIdx) {
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        const getResult = await blackDao.selectBlack(connection, userIdx);
-        connection.release();
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const getResult = await blackDao.selectBlack(connection, userIdx);
+    connection.release();
 
-        return getResult;
-    } catch (err) {
-        logger.error(`getBlackList Provider error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+    return getResult;
+  } catch (err) {
+    logger.error(`getBlackList Provider error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
 // 블랙리스트 상세 조회
 exports.getBlackPlus = async function (userIdx, blackIdx) {
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        const params = [userIdx, blackIdx];
-        const getResult = await blackDao.selectBlackInfo(connection, params);
-        connection.release();
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const params = [userIdx, blackIdx];
+    const getResult = await blackDao.selectBlackInfo(connection, params);
+    connection.release();
 
-        return getResult;
-    } catch (err) {
-        logger.error(`getBlackPlus Provider error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+    return getResult;
+  } catch (err) {
+    logger.error(`getBlackPlus Provider error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
 // 블랙리스트 유/무 체크
 exports.checkBlackIdx = async function (userIdx, blackIdx) {
-    try { 
-        const connection = await pool.getConnection(async (conn) => conn);
-        const params = [userIdx, blackIdx];
-        const blackCheckResult = await blackDao.checkBlackIdx(connection, params);
-        connection.release();
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const params = [userIdx, blackIdx];
+    const blackCheckResult = await blackDao.checkBlackIdx(connection, params);
+    connection.release();
 
-        return blackCheckResult;
-    } catch (err) {
-        logger.error(`checkBlackIdx Provider error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+    return blackCheckResult;
+  } catch (err) {
+    logger.error(`checkBlackIdx Provider error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
--- src/app/HashTag/hashtagProvider.js
+++ src/app/HashTag/hashtagProvider.js
@@ -1,50 +1,51 @@
 const { pool } = require("../../../config/database");
 const { errResponse } = require("../../../config/response");
 const { logger } = require("../../../config/winston");
+const baseResponse = require("../../../config/baseResponseStatus");
 
 const hashtagDao = require("./hashtagDao");
 
 // 해시태그 조회
 exports.getHashTag = async function (userIdx) {
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        
-        const getHashTag = await hashtagDao.checkHashTag(connection, userIdx);
-        connection.release();
-        
-        return getHashTag;
-    } catch (err) {
-        logger.error(`getHashTag Provider error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    const getHashTag = await hashtagDao.checkHashTag(connection, userIdx);
+    connection.release();
+
+    return getHashTag;
+  } catch (err) {
+    logger.error(`getHashTag Provider error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
 // 해당 해시태그를 건 사람들 조회
 exports.getHashUser = async function (hashTag) {
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        
-        const getHashUser = await hashtagDao.selectHashTag(connection, hashTag);
-        connection.release();
-        
-        return getHashUser;
-    } catch (err) {
-        logger.error(`getHashUser Provider error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    const getHashUser = await hashtagDao.selectHashTag(connection, hashTag);
+    connection.release();
+
+    return getHashUser;
+  } catch (err) {
+    logger.error(`getHashUser Provider error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
 exports.countHashTag = async function (userIdx) {
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-    
-        const result = await hashtagDao.countHashTag(connection, userIdx);
-    
-        connection.release();
-    
-        return result;
-    } catch (err) {
-        logger.error(`countHashTag Provider error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    const result = await hashtagDao.countHashTag(connection, userIdx);
+
+    connection.release();
+
+    return result;
+  } catch (err) {
+    logger.error(`countHashTag Provider error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
