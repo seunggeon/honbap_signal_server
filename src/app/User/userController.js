849b2a6ae4af1c9d474989b6c2c0a9ba90618884
seunggeon <tmdrjs9555@naver.com>
2022-07-11 18:34:17 +0900

    phone number verification in Naver Test

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -19,6 +19,35 @@ const kakao = {
   redirectUri: "http://15.164.98.165/auth/kakao/callback",
 };
 
+// Naver 번호 인증 //
+const secret_key = require("../../../config/secret_sms");
+
+const Cache = require('memory-cache');
+const CryptoJS = require('crypto-js');
+
+const date = Date.now().toString();
+const uri = secret_key.NCP_serviceID;
+const secretKey = secret_key.NCP_secretKey;
+const accessKey = secret_key.NCP_accessKey;
+const method = 'POST';
+const space = " ";
+const newLine = "\n";
+const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages`;
+const url2 = `/sms/v2/services/${uri}/messages`;
+
+const  hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+
+hmac.update(method);
+hmac.update(space);
+hmac.update(url2);
+hmac.update(newLine);
+hmac.update(date);
+hmac.update(newLine);
+hmac.update(accessKey);
+
+const hash = hmac.finalize();
+const signature = hash.toString(CryptoJS.enc.Base64);
+
 //controller : 판단 부분.
 /**
  * API No. 1
@@ -319,3 +348,75 @@ passport.use(
     }
   )
 );
+
+
+/*
+ * API Name : 번호 인증 API
+ * [POST] /app/send
+ */
+
+exports.send = async function (req, res) {
+  const phoneNumber = req.body.phoneNumber;
+
+  Cache.del(phoneNumber);
+
+  //인증번호 생성
+  const verifyCode = Math.floor(Math.random() * (999999 - 100000)) + 100000;
+
+  Cache.put(phoneNumber, verifyCode.toString());
+
+  axios({
+    method: method,
+    json: true,
+    url: url,
+    headers: {
+      'Content-Type': 'application/json',
+      'x-ncp-iam-access-key': accessKey,
+      'x-ncp-apigw-timestamp': date,
+      'x-ncp-apigw-signature-v2': signature,
+    },
+    data: {
+      type: 'SMS',
+      contentType: 'COMM',
+      countryCode: '82',
+      from: '01023279226',
+      content: `[Milli] 인증번호 [${verifyCode}]를 입력해주세요.`,
+      messages: [
+        {
+          to: `${phoneNumber}`,
+        },
+      ],
+    }, 
+    })
+  .then(function (res) {
+    res.send(response(baseResponse.SMS_SEND_SUCCESS));
+  })
+  .catch((err) => {
+    if(err.res == undefined){
+      res.send(response(baseResponse.SMS_SEND_SUCCESS));
+    }
+    else res.sned(errResponse(baseResponse.SMS_SEND_FAILURE));
+  });
+};
+
+
+/*
+ * API Name : 번호 인증 API
+ * [POST] /app/verify
+ */
+
+exports.verify = async function (req, res) {
+  const phoneNumber = req.body.phoneNumber;
+  const verifyCode = req.body.verifyCode;
+
+  const CacheData = Cache.get(phoneNumber);
+
+  if (!CacheData) {
+    return res.send(errResponse(baseResponse.FAILURE_SMS_AUTHENTICATION));
+  } else if (CacheData !== verifyCode) {
+      return res.send(errResponse(baseResponse.FAILURE_SMS_AUTHENTICATION));
+  } else {
+    Cache.del(phoneNumber);
+    return res.send(response(baseResponse.SMS_VERIFY_SUCCESS));     
+  }
+};
--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -35,4 +35,11 @@ module.exports = function (app) {
     // 10. 카카오 로그인 API
     app.get('/auth/kakao', passport.authenticate('kakao-login')); 
     app.get('/auth/kakao/callback', passport.authenticate('kakao-login', { failureRedirect: '/auth/kakao', successRedirect: '/home' }));
+
+    // 11. 네이버 번호 인증 API
+
+    // 문자인증 - 전송 API
+    app.post('/app/send', user.send);
+    // 문자인증 - 검증 API
+    app.post('/app/verify', user.verify);
 };
