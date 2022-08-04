0e1d6fe1ffd6a4640fd847d9d9b227a4b349a331
seongddiyong <kodd1102@naver.com>
2022-08-04 20:58:33 +0900

    move to chat

--- index.js
+++ index.js
@@ -15,48 +15,4 @@ if (process.env.NODE_ENV === "development") {
 const webserver = express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
 
-
-/*
-  Socket 통신 부분 : 따로 포트 열지 않고 3001번 포트로 진행
-*/
-var io = require('socket.io')(webserver)
-
-io.sockets.on('connection', (socket, opt) => {
-  socket.emit('message', {msg: 'Welcome' + socket.id});
-
-  socket.on('join', function(roomId, fn){
-    socket.join(roomId, function(){
-      util.log("join", roomId, Object.keys(socket.rooms))
-    });
-  });
-
-  socket.on('leave', function(roomId, fn){
-    socket.leave(roomId, function(){
-      if(fn)
-        fn();
-    });
-  });
-
-  socket.on('rooms', function(fn){
-    if(fn)
-      util.log(Object.keys(socket.rooms));
-  });
-
-  socket.on('message', (data, fn) => {
-    util.log("message >>", data.msg, Object.keys(socket.rooms));
-
-    if(fn)
-      fn(data.msg);
-
-    socket.broadcast.to(data.room).emit('message', {room: data.room, msg : data.msg})
-  });
-
-  socket.on('message-for-one', (socketid, msg, fn) =>
-  {
-    socket.to(socketid).emit('message', {msg : msg})
-  });
-
-  socket.on('disconnecting', function(roomId, fn){
-    util.log("disconnecting >> ", socket.id)
-  })
-});
+module.exports = {webserver};
