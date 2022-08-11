c14faaf936b6030876d428541420027d9de5efd1
wjlee611 <wjlee611@gmail.com>
2022-08-11 21:28:27 +0900

    bug fix: report

--- src/app/Report/reportRoute.js
+++ src/app/Report/reportRoute.js
@@ -2,5 +2,5 @@ module.exports = function (app) {
     const report = require("./reportController");
 
     // 1. 신고 등록
-    app.post('/report', report.postReport);
+    app.post('/report/:userIdx', report.postReport);
 }
