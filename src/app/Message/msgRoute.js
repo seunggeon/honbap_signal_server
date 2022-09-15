60888d305ff5e62a631f3cf1c7dfb82f5b790745
seongddiyong <kodd1102@naver.com>
2022-09-15 21:18:53 +0900

    수정된 메세지

--- src/app/Message/msgRoute.js
+++ src/app/Message/msgRoute.js
@@ -1,20 +1,20 @@
 module.exports = function (app) {
-
+    const jwtMiddleware = require("../../../config/jwtMiddleware");
     const msgController = require("./msgController");
     
     // 쪽지 방 생성
-    app.post("/msg", msgController.createMsgRoom);
+    app.post("/msg", jwtMiddleware, msgController.createMsgRoom);
 
     // 쪽지 방 확인
-    app.get("/msg", msgController.getMsgRoom);
+    app.get("/msg", jwtMiddleware, msgController.getMsgRoom);
 
     // 쪽지 보내기
-    app.post("/msg/:roomId", msgController.sendMsg);
+    app.post("/msg/:roomId", jwtMiddleware, msgController.sendMsg);
     
     // 쪽지 확인
-    app.get("/msg/:roomId", msgController.getMsg);
+    app.get("/msg/:roomId", jwtMiddleware, msgController.getMsg);
 
     // 쪽지 방 삭제
-    app.delete("/msg", msgController.deleteMsg);
+    app.delete("/msg", jwtMiddleware, msgController.deleteMsg);
     
 };
