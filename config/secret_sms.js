1bfe6bba0968744560aa6f9e5606dd9dfb9c7d7f
seunggeon <tmdrjs9555@naver.com>
2023-01-27 18:29:45 +0900

    Delete secret_sms.js

--- config/secret_sms.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'NCP_serviceID' : 'ncp:sms:kr:278823571427:phonenumber-verification',
-  'NCP_accessKey' : 'XVsyyEYLZthodhff3GrD',
-  'NCP_secretKey' : 'qAqK1wFLUr7KIg4KpERqaKL42iBfJUdKA4nIuASo'
-};
